59f63d22b44221bd98f39310516738067b9e55f3
Noppadon Sodram <knopsod@gmail.com>
2018-07-15 04:16:40 +0700

    better Index page message

--- imports/ui/pages/Index.js
+++ imports/ui/pages/Index.js
@@ -5,7 +5,7 @@ import { Jumbotron } from 'react-bootstrap';
 const Index = () => (
   <div className="Index">
     <Jumbotron className="text-center">
-      <h2>A4444</h2>
+      <h2>Have a nice day.</h2>
       <p><Link to="/bets" className="btn btn-success">Welcome</Link></p>
       
     </Jumbotron>
