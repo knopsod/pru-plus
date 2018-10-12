97768945b4f82a0bd5164f73fbc1564608d10d10
Noppadon Sodram <knopsod@gmail.com>
2018-10-13 05:52:10 +0700

    check up, down, permute with +-*

--- imports/ui/components/BetEditorFast.js
+++ imports/ui/components/BetEditorFast.js
@@ -28,6 +28,7 @@ class BetEditorFast extends React.Component {
     this.handleCreatedDateChange = this.handleCreatedDateChange.bind(this);
     this.handleBrokerChange = this.handleBrokerChange.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleChangeV2 = this.handleChangeV2.bind(this);
   }
 
   handleCreatedDateChange(value) {
@@ -272,6 +273,168 @@ class BetEditorFast extends React.Component {
     }
   }
 
+  handleChangeV2(e) {
+    const betMessage = e.target.value;
+    this.setState({
+      ...this.state,
+      betMessage,
+    });
+
+    const arr = betMessage.split(/\r?\n/);
+    console.log(arr);
+    
+    var upsert = {};
+
+    var no = '', 
+      up2 = 0, 
+      down2 = 0, 
+      up3 = 0, 
+      down3 = 0, 
+      permute = 0;
+
+    const createdAt = moment().valueOf(), 
+      broker = this.state.broker, 
+      createdDate = this.state.createdDate.substr(0, 10),
+      userId = Meteor.userId();
+
+    if ( arr.length > 1 && 
+      ( _.includes( arr[arr.length - 2], '+' ) || 
+        _.includes( arr[arr.length - 2], '-' ) ||
+        _.includes( arr[arr.length - 2], '*' )
+      ) 
+    ) {
+      var strBet = arr[arr.length - 2];
+      strBet = strBet.replace(/\+/, ' +');
+      strBet = strBet.replace(/-/, ' -');
+      strBet = strBet.replace(/\*/, ' *');
+      console.log(strBet);
+      
+      var splitBets = strBet.split(' ');
+      console.log(splitBets);
+
+      if (splitBets[0].length === 2 || splitBets[0].length === 3) {
+        no = splitBets[0];
+      }
+
+      for (let i = 1; i < splitBets.length; i++) {
+        switch (splitBets[i].charAt(0)) {
+          case '+':
+            if (splitBets[0].length === 2) up2 = parseInt(splitBets[i].slice(1));
+            if (splitBets[0].length === 3) up3 = parseInt(splitBets[i].slice(1));
+            break;
+
+          case '-':
+            if (splitBets[0].length === 2) down2 = parseInt(splitBets[i].slice(1));
+            if (splitBets[0].length === 3) down3 = parseInt(splitBets[i].slice(1));
+            break;
+
+          case '*':
+            if (splitBets[0].length === 3) permute = parseInt(splitBets[i].slice(1));
+            break;
+        
+          default:
+            break;
+        }
+      }
+    }
+
+    // up2+down2
+    if ( arr.length > 2 &&
+      !isNaN( parseInt(arr[arr.length - 3]) ) && arr[arr.length - 3].length === 2 &&
+      !isNaN( parseInt(arr[arr.length - 2]) ) &&
+      arr[arr.length - 1] === ''
+    ) {
+      console.log('up2+down2');
+      no = arr[arr.length - 3];
+      up2 = down2 = parseInt(arr[arr.length - 2]);
+    }
+
+    // up3+permute
+    if ( arr.length > 2 &&
+      !isNaN( parseInt(arr[arr.length - 3]) ) && arr[arr.length - 3].length === 3 &&
+      !isNaN( parseInt(arr[arr.length - 2]) ) &&
+      arr[arr.length - 1] === ''
+    ) {
+      console.log('up3+down3');
+      no = arr[arr.length - 3];
+      up3 = permute = parseInt(arr[arr.length - 2]);
+    }
+
+    // no2+latestSpend
+    if ( arr.length > 2 &&
+      arr[arr.length - 3].length === 2 &&
+      !isNaN( parseInt( arr[arr.length - 3] ) ) &&
+      arr[arr.length - 2] === "" &&
+      arr[arr.length - 1] === ""
+    ) {
+      console.log('no2+latestSpend');
+
+      if ( this.props.Session.get('latestSessionBet').up3 > 0 || 
+        this.props.Session.get('latestSessionBet').down3 > 0 ||
+        this.props.Session.get('latestSessionBet').permute > 0
+      ) {
+        return;
+      } else {
+        no = arr[arr.length - 3];
+        up2 = this.props.Session.get('latestSessionBet').up2;
+        down2 = this.props.Session.get('latestSessionBet').down2;
+      }
+    }
+
+    // no3+latestSpend
+    if ( arr.length > 2 &&
+      arr[arr.length - 3].length === 3 &&
+      !isNaN( parseInt( arr[arr.length - 3] ) ) &&
+      arr[arr.length - 2] === "" &&
+      arr[arr.length - 1] === ""
+    ) {
+      console.log('no3+latestSpend');
+
+      if ( this.props.Session.get('latestSessionBet').up2 > 0 ||
+        this.props.Session.get('latestSessionBet').down2 > 0 
+      ) {
+        return;
+      } else {
+        no = arr[arr.length - 3];
+        up3 = this.props.Session.get('latestSessionBet').up3;
+        down3 = this.props.Session.get('latestSessionBet').down3;
+        permute = this.props.Session.get('latestSessionBet').permute;
+      }
+    }
+
+    if ( no !== '' ) {
+      upsert = { no, 
+        up2, 
+        down2, 
+        up3, 
+        down3, 
+        permute, 
+        createdAt, 
+        broker, 
+        createdDate,
+        userId,
+      };
+      console.log(upsert);
+
+      upsertBet.call(upsert, (error, response) => {
+        if (error) {
+          Bert.alert(error.reason, 'danger');
+        } else {
+          Bert.alert('Bet added', 'success');
+
+          this.insertedId = response.insertedId;
+          this.props.Session.set('insertedId', response.insertedId);
+        }
+      });
+
+      this.setState({
+        ...this.state,
+        betMessage: '',
+      });
+    }
+
+  }
+
   render() {
 
     const yellowStyle = { minHeight: 150, maxHeight: 225, fontSize: 48, borderColor: 'yellow', borderWidth: 2 };
@@ -302,7 +465,7 @@ class BetEditorFast extends React.Component {
           style={this.state.betMessage.length > 0 ? yellowStyle : redStyle}
           bsSize="large"
           value={this.state.betMessage}
-          onChange={this.handleChange}
+          onChange={this.handleChangeV2}
         />
       </FormGroup>
 
@@ -318,23 +481,23 @@ class BetEditorFast extends React.Component {
         <tbody>
           <tr>
             <td>1.</td>
-            <td>45 = 10x10 (บนxล่าง)</td>
-            <td>45<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong>10<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
-          </tr>
-          <tr>
-            <td>2.</td>
             <td>45 = 10 (บน) หรือ 45 = 10x0</td>
             <td>45+10<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
           </tr>
           <tr>
-            <td>3.</td>
+            <td>2.</td>
             <td>45 = 10 (ล่าง) หรือ 45 = 0x10</td>
             <td>45-10<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
           </tr>
           <tr>
-            <td>4.</td>
+            <td>3.</td>
             <td>45 = 10x5 (บนxล่าง)</td>
             <td>45+10-5<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
+          </tr>
+          <tr>
+            <td>4.</td>
+            <td>45 = 10x10 (บนxล่าง)</td>
+            <td>45<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong>10<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
           </tr>
           <tr>
             <td>5.</td>
@@ -343,11 +506,6 @@ class BetEditorFast extends React.Component {
           </tr>
           <tr>
             <td colSpan={3}></td>
-          </tr>
-          <tr>
-            <td>6.</td>
-            <td>456 = 10x10 (บนxโต๊ด)</td>
-            <td>456<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong>10<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
           </tr>
           <tr>
             <td>7.</td>
@@ -360,14 +518,19 @@ class BetEditorFast extends React.Component {
             <td>456*10<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
           </tr>
           <tr>
+            <td>10.</td>
+            <td>456 = 10 (ล่าง)</td>
+            <td>456-10<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
+          </tr>
+          <tr>
             <td>9.</td>
             <td>456 = 10x5 (บนxโต๊ด)</td>
             <td>456+10*5<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
           </tr>
           <tr>
-            <td>10.</td>
-            <td>456 = 10 (ล่าง)</td>
-            <td>456-10<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
+            <td>6.</td>
+            <td>456 = 10x10 (บนxโต๊ด)</td>
+            <td>456<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong>10<strong style={{ color: 'red', fontSize: 12 }}>[Enter]</strong></td>
           </tr>
           <tr>
             <td>11.</td>
