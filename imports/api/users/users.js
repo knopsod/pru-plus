c45716884abf3b5189df52c8820137c94ebdcaa6
Noppadon Sodram <knopsod@gmail.com>
2018-12-19 08:06:39 +0700

    comment code users.js->Users.schema, users.js->Users.attachSchema(...) ok

--- imports/api/users/users.js
+++ imports/api/users/users.js
@@ -18,22 +18,22 @@ Users.deny({
   remove: () => true,
 });
 
-Users.schema = new SimpleSchema({
-  profile: {
-    type: Object,
-    label: 'The employer of the employment.',
-    optional: true,
-  },
-  'profile.name': { type: Object, optional: true },
-  'profile.name.first': { type: String, optional: true },
-  'profile.name.last': { type: String, optional: true },
-  'profile.lineId': { type: String, optional: true },
-  _id: { type: String, optional: true },
-});
+// Users.schema = new SimpleSchema({
+//   profile: {
+//     type: Object,
+//     label: 'The employer of the employment.',
+//     optional: true,
+//   },
+//   'profile.name': { type: Object, optional: true },
+//   'profile.name.first': { type: String, optional: true },
+//   'profile.name.last': { type: String, optional: true },
+//   'profile.lineId': { type: String, optional: true },
+//   _id: { type: String, optional: true },
+// });
 
-Users.attachSchema(Users.schema);
+// Users.attachSchema(Users.schema);
 
-Factory.define('employment', Users, {
+Factory.define('user', Users, {
   profile: () => 'Factory Profile',
   _id: () => 'Factory ID',
 });
