333a4512104931cb656a8b386d1c310da7d43699
Noppadon Sodram <knopsod@gmail.com>
2019-02-01 22:55:48 +0700

    hide JobsDoneList

--- imports/ui/pages/Jobs.js
+++ imports/ui/pages/Jobs.js
@@ -29,12 +29,12 @@ const Jobs = () => (
         <JobsOnHandList />
       </Col>
     </Row>
-    <Row>
+    {/* <Row>
       <Col xs={ 12 }>
         <span>ให้คะแนนงาน</span>
         <JobsDoneList />
       </Col>
-    </Row>
+    </Row> */}
   </div>
 );
 
